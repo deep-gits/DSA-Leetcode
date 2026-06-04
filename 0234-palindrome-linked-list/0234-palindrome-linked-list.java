a6203b3629ec3ef6c7aa80bf8c4ad556c678a212
Deepak saraswat <deepaksaraswat8913@gmail.com>
2026-06-04 09:52:29 +0530

    06-04-2026

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -12,24 +12,38 @@ class Solution {
         return prev;
         }
     public boolean isPalindrome(ListNode head) {
-        if(head.next==null) return true;
-        //create a deep copy
-        ListNode newhead=new ListNode(head.val);
-        ListNode t1 =head;
-        ListNode  t2=newhead;
-        while(t1!=null){
-            ListNode temp =new ListNode(t1.val);
-            t2.next=temp;
-            t2=t2.next;
-            t1=t1.next;
+        // if(head.next==null) return true;
+        // //create a deep copy
+        // ListNode newhead=new ListNode(head.val);
+        // ListNode t1 =head;
+        // ListNode  t2=newhead;
+        // while(t1!=null){
+        //     ListNode temp =new ListNode(t1.val);
+        //     t2.next=temp;
+        //     t2=t2.next;
+        //     t1=t1.next;
+        // }
+        // newhead=reverseList(newhead);
+        // t1=head;
+        // t2=newhead;
+        // while(t1!=null){
+        //     if(t1.val!=t2.val) return false;
+        //     t2=t2.next;
+        //     t1=t1.next;
+        // }
+        // return true;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        newhead=reverseList(newhead);
-        t1=head;
-        t2=newhead;
-        while(t1!=null){
-            if(t1.val!=t2.val) return false;
-            t2=t2.next;
-            t1=t1.next;
+        ListNode j=reverseList(slow);
+        ListNode i=head;
+        while(j!=null){
+            if(i.val!=j.val) return false;
+            i=i.next;
+            j=j.next;
         }
         return true;
     }
