3d763c4ac2d05fc081474ffcec2b4ca4032cb797
Deepak saraswat <deepaksaraswat8913@gmail.com>
2026-06-06 08:08:08 +0530

    06-06-2026

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,58 +1,26 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
-        if(l1 == null || l2 == null){
-            return l1 == null ? l2 : l1;
-        }
-        
-        ListNode result = new ListNode();
-        ListNode ans = result;
+        ListNode  dummy =new ListNode(-1);
+        ListNode temp=dummy;
         int carry=0;
-        while(l1 != null && l2 != null){
-            
-            int sum = carry + l1.val + l2.val;
-            int digit = sum%10;
-            carry = sum/10;
-            
-            l1 = l1.next;
-            l2 = l2.next;
-            
-           result.next = new ListNode(digit);   
-            result = result.next;
+        while (l1!=null||l2!=null){
+            int val1=0,val2=0;
+            if(l1!=null) val1=l1.val;
+            if(l2!=null) val2=l2.val;
+            int num=val1+val2+carry;
+            ListNode node=new ListNode(num%10);
+            temp.next=node;
+            temp=temp.next;
+            if(num>9) carry=1;
+            else carry=0;
+            if(l1!=null) l1=l1.next;
+            if(l2!=null) l2=l2.next;
         }
-        
-        while(l1 != null){
-            int sum = carry + l1.val;
-            int digit = sum%10;
-            carry = sum/10;
-            result.next = new ListNode(digit);
-            l1 = l1.next;
-            result = result.next;
-        } 
-        
-        while(l2 != null){
-            int sum = carry + l2.val;
-            int digit = sum%10;
-            carry = sum/10;
-            result.next = new ListNode(digit);
-            l2 = l2.next;
-            result = result.next;
-        } 
-        
-        if(carry != 0){
-            result.next = new ListNode(carry);
+        if(carry==1){
+            ListNode node =new ListNode(1);
+            temp.next=node;
+            temp=temp.next;
         }
-        
-        return ans.next;
+        return dummy.next;
     }
 }
